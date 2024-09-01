148f061664868161e6a7f2d0354ded0cc3c6374c
fl-oneto <fl.oneto@duocuc.cl>
2024-09-01 19:58:57 -0400

    se agregan elementos a page horario

--- src/app/pages/horario/horario.page.ts
+++ src/app/pages/horario/horario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-horario',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./horario.page.scss'],
 })
 export class HorarioPage implements OnInit {
+  presentingElement: Element | null = null;
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page');
   }
 
+  canDismiss = async () => {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: '¿Estás seguro?',
+      buttons: [
+        {
+          text: 'Sí',
+          role: 'confirm',
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+      ],
+    });
+    await actionSheet.present();
+    
+    const { role } = await actionSheet.onWillDismiss();
+    return role === 'confirm';
+  };
 }
