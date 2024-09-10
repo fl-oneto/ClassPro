188089f838abf936d7212b8749e6bf9eb7270f92
fl-oneto <fl.oneto@duocuc.cl>
2024-09-09 22:11:57 -0300

    se agregan validaciones

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -20,36 +20,39 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  ingresar(){
-    if(this.validateModel(this.login)){
-      //agrego creación de parámetros
-      let navigationExtras : NavigationExtras = {
-        state:{login: this.login}
+  ingresar() {
+    if (this.validateModel(this.login)) {
+      // Si el modelo es válido, navegar a la página de inicio
+      let navigationExtras: NavigationExtras = {
+        state: { login: this.login }
       };
       this.router.navigate(['/home'], navigationExtras);
-      this.presentToast("top","Bienvenido",5000)
-    }else{
-      this.presentToast("middle","Error - Falta: "+this.field);
-    }    
+      this.presentToast("middle", "Bienvenido", 2000);
+    }
   }
-
-  /**
-   * validateModel para validar el ingreso de algo en los
-   * campos de mi html mediante el modelo login
-   */
-  validateModel(model:any){
-    //Recorro todas las entradas que me entrega el Object entries obteniendo
-    //el par key : value
-    for(var [key ,value] of Object.entries(model)){
-      //reviso si value = "" y retorno false e indico campo faltante
-      if(value == ""){
-        this.field = key;
+  
+  validateModel(model: any) {
+    for (var [key, value] of Object.entries(model)) {
+      const stringValue = value as string;
+  
+      if (key === 'password') {
+        if (stringValue === "") {
+          this.presentToast("middle", "El campo password no puede estar vacío.");
+          return false;
+        } else if (stringValue.length < 8) {
+          this.presentToast("middle", "La contraseña debe tener al menos 8 caracteres.");
+          return false;
+        }
+      } else if (stringValue === "") {
+        this.presentToast("middle", "El campo " + key + " no puede estar vacío.");
         return false;
       }
-    } 
-    //si termina el for es que los valores fueron ingresados
+    }
     return true;
   }
+  
+  
+
 
   async presentToast(position: 'top' | 'middle' | 'bottom', msg:string, duration?:number) {
     const toast = await this.toastController.create({
