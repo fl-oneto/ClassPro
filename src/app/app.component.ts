5a51888353d863a4dd9776851f1cf55905495694
Guonejo <ma.carvachos@duocuc.cl>
2024-10-20 01:16:13 -0300

    Splash Screen + Android build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
